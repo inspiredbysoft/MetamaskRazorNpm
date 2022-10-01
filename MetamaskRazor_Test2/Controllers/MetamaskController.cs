73b91078edd5fdfafb6383730e79a2c5cba5e865
Maciej Czestochowski <maciej@onvard.de>
2022-10-01 17:50:28 +0200

    Added basic files

---
+++ MetamaskRazor_Test2/Controllers/MetamaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetamaskRazor_Test2.Controllers
+{
+    public class MetamaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MetamaskRazor_Test2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MetamaskRazor_Test2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
